e4be9a08db26ea400eda02d53c3ad649cb313e11
magotheinnocent <innocentp576@gmail.com>
2020-12-15 21:51:21 +0300

    Initial commit

---
+++ Problems/The army of units/main.py
@@ -0,0 +1,18 @@
+units = int(input())
+if units < 1:
+    print("no army")
+
+elif 1 >= units <= 9:
+    print("few")
+
+elif 10 >= units <= 49:
+    print("pack")
+
+elif 50 >= units <= 499:
+    print("horde")
+
+elif 500 >= units <= 999:
+    print("swarm")
+
+else:
+    print("legion")
