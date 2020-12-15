d71c21fbf47a5dde4eecdf9ff24108de25c9a738
magotheinnocent <innocentp576@gmail.com>
2020-12-15 20:38:53 +0300

    Initial commit

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,7 @@
 # write your code here
-print("X O X")
-print("O X O")
-print("X X O")
+number = str(input("Enter cells:"))
+print("---------")
+print(f"| {number[0]} {number[1]} {number[2]} |")
+print(f"| {number[3]} {number[4]} {number[5]} |")
+print(f"| {number[6]} {number[7]} {number[8]} |")
+print("---------")
