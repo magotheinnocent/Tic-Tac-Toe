db4d698b6a42612a2f14caca52b597fecfe7b5f8
magotheinnocent <innocentp576@gmail.com>
2020-12-16 10:56:53 +0300

    Initial commit

--- Problems/Long live the king/main.py
+++ Problems/Long live the king/main.py
@@ -4,7 +4,7 @@ row = int(input())
 if (column == 1 and row == 1) or (column == 8 and row == 1) or (column == 8 and row == 8) or (column == 1 and row == 8):
     print("3")
 
-elif (column == 1 and 1 < row < 8) or (1 < column < 8 and row == 1) or (column == 8 and 1 < row < 8) or(1 < column < 8 and row == 8):
+elif (column == 1 and 1 < row < 8) or (row == 1 and 1 < column < 8) or (column == 8 and 1 < row < 8) or (row == 8 and 1 < column < 8):
     print("5")
 
 else:
