fcfec4740b4d557e04888b24eb078101628c5ad9
magotheinnocent <innocentp576@gmail.com>
2020-12-16 10:24:48 +0300

    Initial commit

--- Problems/Coordinates/main.py
+++ Problems/Coordinates/main.py
@@ -1,16 +1,16 @@
 number1 = input()
 number2 = input()
 
-if number1 == 0 and number2 == 0:
+if float(number1) == 0.0 and float(number2) == 0.0:
     print("Origin")
 
-elif number1 > 0 and number2 > 0:
+elif float(number1) > 0.0 and float(number2) > 0.0:
     print("I")
 
-elif number1 < 0 and number2 > 0:
+elif float(number1) < 0.0 and float(number2) > 0.0:
     print("II")
 
-elif number1 < 0 and number2 < 0:
+elif float(number1) < 0.0 and float(number2) < 0.0:
     print("III")
 
 else:
