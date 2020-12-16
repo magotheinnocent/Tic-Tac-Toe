bf463a346091b4ccc0cc279ff1d585438afc3f73
magotheinnocent <innocentp576@gmail.com>
2020-12-16 14:49:07 +0300

    Initial commit

--- Problems/Vowel count/main.py
+++ Problems/Vowel count/main.py
@@ -1,7 +1,8 @@
 string = "red yellow fox bite orange goose beeeeeeeeeeep"
-vowels = ['a', 'e', 'i', 'o', 'u']
-number = 0
-for vowels in string:
-   number += 1
+vowels = 'aeiou'
+vowel_count = 0
 
-print(number)
+for value in string:
+    if value in vowels:
+        vowel_count += 1
+print(vowel_count)
