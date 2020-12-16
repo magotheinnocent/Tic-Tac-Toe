b4576c3f5a2030cf41b8d390c1fc480a573cf6a8
magotheinnocent <innocentp576@gmail.com>
2020-12-16 11:02:26 +0300

    Initial commit

--- Problems/Menu/main.py
+++ Problems/Menu/main.py
@@ -2,4 +2,15 @@ pizza = ("Margherita", "Four Seasons", "Neapolitan", "Vegetarian", "Spicy")
 salad = ("Caesar salad", "Green salad", "Tuna salad", "Fruit salad")
 soup = ("Chicken soup", "Ramen", "Tomato soup", "Mushroom cream soup")
 
-if
+choice = input()
+if choice in pizza:
+    print(pizza)
+
+elif choice in salad:
+    print(salad)
+
+elif choice in soup:
+    print(soup)
+
+else:
+    print("Sorry, we don't have it in the menu")
