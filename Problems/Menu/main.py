514c3fe61f6a4e681696a133da002851fc606cdd
magotheinnocent <innocentp576@gmail.com>
2020-12-16 11:10:53 +0300

    Initial commit

--- Problems/Menu/main.py
+++ Problems/Menu/main.py
@@ -1,4 +1,4 @@
-pizza = ("Margherita", "Four Seasons", "Neapolitan", "Vegetarian", "Spicy")
+pizza = (Margherita, "Four Seasons", "Neapolitan", "Vegetarian", "Spicy")
 salad = ("Caesar salad", "Green salad", "Tuna salad", "Fruit salad")
 soup = ("Chicken soup", "Ramen", "Tomato soup", "Mushroom cream soup")
 
