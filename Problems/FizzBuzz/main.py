2e8cee1459bef63c1b3c1d9d9bf532f8be305aff
magotheinnocent <innocentp576@gmail.com>
2020-12-17 20:57:18 +0300

    Initial commit

--- Problems/FizzBuzz/main.py
+++ Problems/FizzBuzz/main.py
@@ -1,13 +1,11 @@
-a = int(input())
-b = int(input())
-mod = []
+
 for i in range(1, 101):
     if i % 3 == 0:
-        print(mod.append(i))
+        print(i)
         print("Fizz")
     elif i % 5 == 0:
-        print(mod.append(i))
+        print(i)
         print("Buzz")
     elif i % 5 == 0 and i % 3 == 0:
-        print(mod.append(i))
+        print(i)
         print("FizzBuzz")
