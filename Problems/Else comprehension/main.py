4c3f916474c3c9759904c6150666447205d51d20
magotheinnocent <innocentp576@gmail.com>
2021-02-10 11:00:16 +0300

    Initial commit

--- Problems/Else comprehension/main.py
+++ Problems/Else comprehension/main.py
@@ -1,5 +1,5 @@
 # the following line reads the list from the input, do not modify it, please
 old_list = [int(num) for num in input().split()]
 
-binary_list = [x for x in old_list if old_list[0] > 0]
+binary_list = [x for x in old_list if old_list > 0]
 print(binary_list)
