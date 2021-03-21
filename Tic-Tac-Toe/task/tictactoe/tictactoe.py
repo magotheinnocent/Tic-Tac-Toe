978cef1ed8483cd1b455fdb8e5aabe6f8003af06
magotheinnocent <innocentp576@gmail.com>
2021-03-21 10:47:02 +0300

    Initial commit

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -7,14 +7,15 @@ print(f"| {number[3]} {number[4]} {number[5]} |")
 print(f"| {number[6]} {number[7]} {number[8]} |")
 print("---------")
 
-x_win = {number[0]} == {number[1]} == {number[2]} == "X" or {number[3]} == {number[4]} == {number[5]} == "X" or {number[6]} == {number[7]} == {number[8]} == "X"
-o_win = {number[0]} == {number[1]} == {number[2]} == "O" or {number[3]} == {number[4]} == {number[5]} == "O" or {number[6]} == {number[7]} == {number[8]} == "O"
+x_win = {number[0]} == {number[1]} == {number[2]} == "X" \
+        or {number[3]} == {number[4]} == {number[5]} == "X" \
+        or {number[6]} == {number[7]} == {number[8]} == "X"
+o_win = {number[0]} == {number[1]} == {number[2]} == "O" \
+        or {number[3]} == {number[4]} == {number[5]} == "O" \
+        or {number[6]} == {number[7]} == {number[8]} == "O"
 
 if x_win:
     print("X wins")
 
 elif o_win:
     print("O wins")
-
-
-x
